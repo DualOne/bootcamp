293769cde5ae200f7f32bdae8f94ea5d0c0835b7
Zakharenko, Yaroslav <Yaroslav.Zakharenko@billing.ru>
2020-04-18 23:46:54 +0700

    Refactor user ID generation servlet to token generation Add missing response statuses Add Gradle build files to .gitignore

--- src/main/java/com/nexign/bootcamp/token_service/UserTokenGenerationServlet.java
+++ src/main/java/com/nexign/bootcamp/token_service/UserTokenGenerationServlet.java
@@ -1,28 +1,31 @@
-package com.nexign.bootcamp;
+package com.nexign.bootcamp.token_service;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.UUID;
 
-public class UserIdServlet extends HttpServlet {
+public class UserTokenGenerationServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-        String username = req.getParameter("user");
-
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
+
+        String username = req.getParameter("user");
         try (PrintWriter writer = resp.getWriter()) {
             if (username != null && !username.isEmpty()) {
-                int userId = username.hashCode();
-                writer.println(String.format("{\"user\": \"%s\", \"id\": %d}", username, userId));
+                writer.println(String.format("{\"user\": \"%s\", \"token\": %s}",
+                        username, UUID.nameUUIDFromBytes(username.getBytes()).toString()));
             } else {
                 writer.println("{\"error\": \"Username is empty\"}");
+                resp.setStatus(400);
                 System.err.println("No username provided");
             }
         } catch (IOException ex) {
+            resp.setStatus(500);
             System.err.println(String.format("Error printing result: %s", ex.getMessage()));
         }
     }
