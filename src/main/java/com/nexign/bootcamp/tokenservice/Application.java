8e8870f1d3be189bce39acfdd30990421dba1f22
Zakharenko, Yaroslav <yaroslav.zakharenko@nexign.com>
2021-02-26 17:14:26 +0700

    Minor refactoring

--- src/main/java/com/nexign/bootcamp/tokenservice/Application.java
+++ src/main/java/com/nexign/bootcamp/tokenservice/Application.java
@@ -3,10 +3,8 @@ package com.nexign.bootcamp.tokenservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
